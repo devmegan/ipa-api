9ffff5ded6124db095bd3d353dd8406f25b5bac8
devmegan <devmegan@protonmail.com>
2023-02-22 12:12:41 +0000

    add: letter path param validation

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"instructions": "use the /letter/{letter} endpoint to fetch the corresponding phonetic code word"}
+    return {
+        "instructions": "use the /letter/{letter} endpoint to fetch the corresponding phonetic code word"
+    }
 
 
 @app.get("/letter/{letter}")
-def read_item(letter: str):
+def read_item(letter: str = Path(max_length=1, regex="^[a-zA-Z]$")):
     letter = letter.upper()
 
     file = open('ipa.json')
